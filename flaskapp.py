086ed7cdef70152f1bb4264f9e30d6b590d34e87
Ethan Steininger <ethan@Ethans-MacBook-Pro.local>
2017-06-18 14:03:11 -0400

    Duh

--- flaskapp.py
+++ flaskapp.py
@@ -13,6 +13,10 @@ def home():
     query = db.test1.find()
     return render_template('index.html', query=query)
 
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    return redirect("/", code=302)
+
 @app.route('/pi')
 def pi():
     return render_template('pi.html')
