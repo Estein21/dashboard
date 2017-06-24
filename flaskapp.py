e0fd6755389f891460e7c8788e2dccc803b7d4f7
Ethan Steininger <ethan@Ethans-MacBook-Pro.local>
2017-06-24 09:11:21 -0400

    update

--- flaskapp.py
+++ flaskapp.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request, jsonify, redirect, make_response, session
+from flask import Flask, render_template, request, jsonify, redirect, make_response, session, url_for
 from pymongo import MongoClient, GEO2D
+from werkzeug.utils import secure_filename
 from collections import Counter
 from jinja2 import Template
-import json
+from util import Util
 
 app = Flask(__name__)
 
-db = MongoClient('13.58.90.190', 27017).test
+u = Util()
+db = u.dbConfig()
 
 @app.route('/')
 def home():
-    query = db.test1.find()
-    return render_template('index.html', query=query)
+    query = db.test1.find({})
+    return render_template('index.html', query = query)
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
-    return redirect("/", code=302)
+@app.route('/upload')
+def upload_page():
+    return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ util.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient, GEO2D
+
+mindBodyKey = 'he1mbH0lJxucXKUPpWrvT5vIN1c='
+
+
+
+# def GetClientsByString(self, searchStr):
+#         request = self.CreateBasicRequest("GetClientsRequest")
+#
+#         request.SearchText = searchStr
+#         request.CurrentPageIndex = 1 # increase this number by one each time
+#
+#         return self.service.service.GetClients(request)
+
+class Util:
+    def __init__(self):
+        pass
+    def dbConfig(self):
+        db = MongoClient('13.58.90.190', 27017).test
+        return db
