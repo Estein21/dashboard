b851d47d67ab9faf4cdd7b6b6d489e285777a528
Ethan Steininger <ethan@Ethans-MacBook-Pro.local>
2017-07-04 15:07:39 -0400

    Working

--- __init__.py
+++ __init__.py
@@ -80,10 +80,31 @@ def do_admin_login():
 def get_classes():
     service = ClassServiceCalls()
     response = service.GetClasses()
+    #======
+    from suds.sudsobject import asdict
 
-    required_dict = Client.dict(response)
-    print required_dict
-    return ''
+    def recursive_asdict(d):
+        """Convert Suds object into serializable format."""
+        out = {}
+        for k, v in asdict(d).iteritems():
+            if hasattr(v, '__keylist__'):
+                out[k] = recursive_asdict(v)
+            elif isinstance(v, list):
+                out[k] = []
+                for item in v:
+                    if hasattr(item, '__keylist__'):
+                        out[k].append(recursive_asdict(item))
+                    else:
+                        out[k].append(item)
+            else:
+                out[k] = v
+        return out
+
+    def suds_to_json(data):
+        return json.dumps(recursive_asdict(data), default=json_util.default)
+    #========
+
+    return suds_to_json(response)
 
 @app.route("/logout")
 def logout():
