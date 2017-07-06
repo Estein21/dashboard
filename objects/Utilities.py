3ddcd9aa765b1616491178756d88991eafe81493
Ethan Steininger <ethan@Ethans-MacBook-Pro.local>
2017-07-05 21:22:43 -0400

    Go

---
+++ objects/Utilities.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient, GEO2D
+ 
+class Utilities:
+    def __init__(self):
+        pass
+
+    def dbConfig(self):
+        db = MongoClient('52.15.58.213', 27017).test
+        return db
+
+    def secretKey(self):
+        key = 'akshdjasdGHJsslkgajh'
+        return key
